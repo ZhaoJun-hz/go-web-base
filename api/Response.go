c3ba815c453a380c9b789455136a2e3e9d8e4122
zhaojun <zhaojuncodeing@163.com>
2024-06-13 22:37:47 +0800

    封装返回结果

---
+++ api/Response.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"reflect"
+)
+
+type Response struct {
+	Status int    `json:"-"`
+	Code   int    `json:"code,omitempty"`
+	Msg    string `json:"msg,omitempty"`
+	Data   any    `json:"data,omitempty"`
+}
+
+func (m Response) IsEmpty() bool {
+	return reflect.DeepEqual(m, Response{})
+}
+
+func buildStatus(resp Response, defaultStatus int) int {
+	if resp.Status == 0 {
+		return defaultStatus
+	}
+	return resp.Status
+}
+
+func OK(ctx *gin.Context, resp Response) {
+	HttpResponse(ctx, buildStatus(resp, http.StatusOK), resp)
+}
+
+func Fail(ctx *gin.Context, resp Response) {
+	HttpResponse(ctx, buildStatus(resp, http.StatusBadRequest), resp)
+}
+
+func ServerFail(ctx *gin.Context, resp Response) {
+	HttpResponse(ctx, buildStatus(resp, http.StatusInternalServerError), resp)
+}
+
+func HttpResponse(ctx *gin.Context, status int, resp Response) {
+	if resp.IsEmpty() {
+		ctx.AbortWithStatus(status)
+		return
+	}
+	ctx.AbortWithStatusJSON(status, resp)
+}
