242c2b6eb6fa08292d689903f0e054361743043c
zhaojun <zhaojuncodeing@163.com>
2024-06-13 22:09:31 +0800

    jwt相关

---
+++ utils/jwt.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/spf13/viper"
+	"time"
+)
+
+var signingKey = viper.GetString("jwt.signingKey")
+
+type JwtCustomClaims struct {
+	ID int
+	jwt.RegisteredClaims
+}
+
+func GeneratorToken(id int) (string, error) {
+	jwtCustomClaims := JwtCustomClaims{
+		ID: id,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(viper.GetDuration("jwt.tokenExpire") * time.Minute)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Subject:   "Token",
+		}}
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtCustomClaims)
+	return jwtToken.SignedString([]byte(signingKey))
+}
+
+func ParseToken(tokenString string) (JwtCustomClaims, error) {
+	jwtCustomClaims := JwtCustomClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &jwtCustomClaims, func(token *jwt.Token) (interface{}, error) {
+		return signingKey, nil
+	})
+
+	if err == nil && !token.Valid {
+		err = errors.New("invalid token")
+	}
+	return jwtCustomClaims, err
+}
+
+// token 是否是正确的，true 正确，false 错误
+func TokenValid(tokenString string) bool {
+	_, err := ParseToken(tokenString)
+	if err != nil {
+		return false
+	}
+	return true
+}
